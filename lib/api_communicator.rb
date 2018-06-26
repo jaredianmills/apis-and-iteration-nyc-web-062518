c0da98226ea995fdfe48e3fb5e3f772add1afef5
jaredianmills <jaredianmills@gmail.com>
2018-06-26 17:44:43 -0400

    add functionality to output a list of movies any Star Wars character is in.

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,29 +2,52 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-def get_character_movies_from_api(character)
-  #make the web request
-  all_characters = RestClient.get('http://www.swapi.co/api/people/')
-  character_hash = JSON.parse(all_characters)
-  
-  # iterate over the character hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
+def correct_url?(url, character)
+  character_hash = RestClient.get(url)
+  character_hash = JSON.parse(character_hash)
+  character_list = character_hash["results"].map do |individual_character|
+    individual_character["name"].downcase
+  end
+  character_list.include?(character.downcase)
 end
 
-def parse_character_movies(films_hash)
-  # some iteration magic and puts out the movies in a nice list
+def get_correct_url(character)
+  counter = 1
+  url = "https://swapi.co/api/people/?page=#{counter}"
+  until correct_url?(url, character)
+    counter +=1
+    url = "https://swapi.co/api/people/?page=#{counter}"
+  end
+  url
+end
+
+def get_individual_character_hash(character)
+  url = get_correct_url(character)
+  character_hash = RestClient.get(url)
+  character_hash = JSON.parse(character_hash)
+  character_hash["results"].find do |individual_character|
+    individual_character["name"].downcase == character
+  end
+end
+
+def get_character_movies_from_api(character)
+  #make the web request
+  individual_character_hash = get_individual_character_hash(character)
+  individual_character_hash["films"]
+end
+
+
+def parse_character_movies(films)
+  films.each do |movie|
+    movie_hash = RestClient.get(movie)
+    movie_hash = JSON.parse(movie_hash)
+    puts movie_hash["title"]
+  end
 end
 
 def show_character_movies(character)
-  films_hash = get_character_movies_from_api(character)
-  parse_character_movies(films_hash)
+  films = get_character_movies_from_api(character)
+  parse_character_movies(films)
 end
 
 ## BONUS
